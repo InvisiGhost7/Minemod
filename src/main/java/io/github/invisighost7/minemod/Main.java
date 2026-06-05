a6846b06c4747bf9b38e9cf8ca3e97b7d553ba80
Leo Pan <leopannl@outlook.com>
2026-06-05 13:47:55 +0200

    Update

--- src/main/java/io/github/invisighost7/minemod/Main.java
+++ src/main/java/io/github/invisighost7/minemod/Main.java
@@ -3,7 +3,10 @@ package io.github.invisighost7.minemod;
 import net.fabricmc.api.ModInitializer;
 
 public class Main implements ModInitializer {
+	public static final String MOD_ID = "minemod";
+
 	@Override
 	public void onInitialize() {
+		ModBlocks.initialize();
 	}
 }
---
+++ src/main/java/io/github/invisighost7/minemod/ModBlocks.java
@@ -0,0 +1,41 @@
+package io.github.invisighost7.minemod;
+
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+
+import java.util.function.Function;
+
+public class ModBlocks {
+
+    public static <B extends Block> B register(String name, Function<Block.Properties, B> blockFactory, Block.Properties settings) {
+        ResourceKey<Block> blockKEY = ResourceKey.create(Registries.BLOCK, Identifier.fromNamespaceAndPath(Main.MOD_ID, name));
+
+        B block = blockFactory.apply(settings.setId(blockKEY));
+
+        Registry.register(BuiltInRegistries.BLOCK, blockKEY, block);
+
+        ResourceKey<Item> itemKey = ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(Main.MOD_ID, name));
+        BlockItem blockItem = new BlockItem(block, new Item.Properties().setId(itemKey).useBlockDescriptionPrefix());
+        Registry.register(BuiltInRegistries.ITEM, itemKey, blockItem);
+
+        return block;
+
+
+    }
+    public static final Block GLOW_GLASS = register("glow_glass", Block:: new,
+            BlockBehaviour.Properties.ofFullCopy(Blocks.GLASS).lightLevel(state -> 15));
+    public static void initialize() {
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.INGREDIENTS)
+                .register((creativeTab) -> creativeTab.accept(ModBlocks.GLOW_GLASS));
+    }
+}
